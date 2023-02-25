b05cb93f0534c35188210eb8bc0a4cb3040f1991
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-02-25 15:28:16 +0300

    routes added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Instructors from "./pages/Instructors";
+import NotFound from "./pages/NotFound";
+import Nav from "./components/Nav"
+import Footer from "./components/Footer"
 
 function App() {
+return(
 
+    <div className="container">
+        <Nav/>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/instructors" element={<Instructors/>}/>
+            <Route path="contact" element={<Contact/>}/>
+            <Route path="*" element={<NotFound/>}/>
+           
+        </Routes>
+       <Footer/>
+    </div>
+);
 }
 
 export default App;
