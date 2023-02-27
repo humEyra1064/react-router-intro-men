76ac63925b330f693f810f411f6bbb0c5d242790
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-02-27 11:29:13 +0300

    components added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
-import Instructors from "./pages/Instructors";
+
 import NotFound from "./pages/NotFound";
 import Nav from "./components/Nav"
 import Footer from "./components/Footer"
@@ -14,7 +14,7 @@ return(
         <Nav/>
         <Routes>
             <Route path="/" element={<Home/>}/>
-            <Route path="/instructors" element={<Instructors/>}/>
+          
             <Route path="/contact" element={<Contact/>}/>
             <Route path="*" element={<NotFound/>}/>
            
